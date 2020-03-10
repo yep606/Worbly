2fc1366712c270744e34120d51d544531826054c
yep606 <ilya.rogov.vladislav@gmail.com>
2020-03-10 19:59:42 +0300

    Tentative preparations before the chat rooms

--- src/main/java/base/controller/ChatController.java
+++ src/main/java/base/controller/ChatController.java
@@ -1,28 +1,45 @@
 package base.controller;
 
 import base.domain.ChatMessage;
+import base.listener.WebSocketEventListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
+
+import static java.lang.String.format;
 
 @Controller
 public class ChatController {
 
-    @MessageMapping("/chat.sendMessage")
-    @SendTo("/topic/public")
-    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
-        return chatMessage;
+    private static final Logger logger = LoggerFactory.getLogger(WebSocketEventListener.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+
+    @MessageMapping("/chat/{roomId}/sendMessage")
+    public void sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
+        messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
     }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage,
-                               SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
+    @MessageMapping("/chat/{roomId}/addUser")
+    public void addUser(@DestinationVariable String roomId, @Payload ChatMessage chatMessage,
+                        SimpMessageHeaderAccessor headerAccessor) {
+        String currentRoomId = (String) headerAccessor.getSessionAttributes().put("room_id", roomId);
+        if (currentRoomId != null) {
+            ChatMessage leaveMessage = new ChatMessage();
+            leaveMessage.setType(ChatMessage.MessageType.LEAVE);
+            leaveMessage.setSender(chatMessage.getSender());
+            messagingTemplate.convertAndSend(format("/channel/%s", currentRoomId), leaveMessage);
+        }
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        return chatMessage;
+        messagingTemplate.convertAndSend(format("/channel/%s", roomId), chatMessage);
     }
 
 }
